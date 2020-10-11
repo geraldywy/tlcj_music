b06a92b26f4952a69597d5b71258867db7d58421
tabbycat01 <yipgerald@yahoo.com.sg>
2020-10-12 01:16:11 +0800

    Updates

---
+++ precache-manifest.1badcdb36215acab69006beb3c20b2ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0773b6aab5c1a9defb9adf18355e0ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c507cc3ea779cfac168",
+    "url": "/static/css/main.6a242927.chunk.css"
+  },
+  {
+    "revision": "b0ccbf1f68120764fb39",
+    "url": "/static/js/2.fe6c0601.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fe6c0601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c507cc3ea779cfac168",
+    "url": "/static/js/main.18b2a210.chunk.js"
+  },
+  {
+    "revision": "9b0c668e6446772f6322",
+    "url": "/static/js/runtime-main.b645032f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
