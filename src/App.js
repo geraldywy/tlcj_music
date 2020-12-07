aedf859ef6fc4c429bb5e59f95d57d41214bb762
tabbycat01 <yipgerald@yahoo.com.sg>
2020-12-07 23:23:53 +0800

    fading backgrounds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import MediaQuery from "react-responsive"
+
+import beach1 from "./images/beach1.jpg"
+import beach2 from "./images/beach2.jpg"
+import beach3 from "./images/beach3.jpg"
+import beach4 from "./images/beach4.jpg"
+import beach5 from "./images/beach5.jpg"
+import beach6 from "./images/beach6.jpg"
+import beach7 from "./images/beach7.jpg"
+import beach8 from "./images/beach8.jpg"
+
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fadingBackgrounds">
+      <img src={beach1} className="f1" />
+      <img src={beach2} className="f2" />
+      <img src={beach3} className="f3" />
+      <img src={beach4} className="f4" />
+      <img src={beach5} className="f5" />
+      <img src={beach6} className="f6" />
+      <img src={beach7} className="f7" />
+      <img src={beach8} className="f8" />
     </div>
   );
 }
