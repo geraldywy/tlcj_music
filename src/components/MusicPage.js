08eddd9c1d318f68bafc81330dcc831759b9c739
tabbycat01 <yipgerald@yahoo.com.sg>
2020-12-16 20:16:37 +0800

    music page frontend update

--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -4,7 +4,7 @@ import MediaQuery from "react-responsive"
 import video1 from "../videos/video1.mp4"
 import VideoContainer from "./VideoContainer"
 
-import "../fonts.css"
+import "../fonts.css";
 
 const MusicPage = () => {
 
@@ -38,8 +38,14 @@ const MusicPage = () => {
         id: "2QKARCPO3sA",
         type: "youtube"
     }
+    
+    const fourthVideo = {
+        id: "hj3h4Hdj2U8",
+        type: "youtube"
+    }
 
-    const clipList = [firstVideo, secondVideo, thirdVideo]
+    // max 3 pinned videos
+    const clipList = [firstVideo, thirdVideo, fourthVideo]
 
     return (
         <div className="musicPage">
@@ -54,8 +60,34 @@ const MusicPage = () => {
             <div className="musicPageContent">
                 <h1 className="reelTitle">Pinned</h1>
                 <VideoContainer clips={clipList} />
-            </div>
 
+                <div className="colContainer">
+                    <h3 className="colTitle">Originals</h3>
+                    <ul className="songTitle">
+                        {[1, 2, 3, 4, 5, 6, 7].map(i => 
+                            <li className="indivTitle" key={i}>
+                                <a href="https://www.youtube.com/watch?v=hj3h4Hdj2U8" target="_blank" rel="noopener noreferrer">
+                                    Worth It - Kodaline (Acoustic Cover)
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+
+                <div className="colContainer">
+                    <h3 className="colTitle">Covers</h3>
+                    <ul className="songTitle">
+                        {[1, 2, 3, 4, 5, 6, 7].map(i => 
+                            <li className="indivTitle" key={i}>
+                                <a href="https://www.youtube.com/watch?v=hj3h4Hdj2U8" target="_blank" rel="noopener noreferrer">
+                                    Worth It - Kodaline (Acoustic Cover)
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+            
             
         </div>
 
