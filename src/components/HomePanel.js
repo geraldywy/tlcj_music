843d7a8965e9e3efdeb35ae2179a418fe761d56a
tabbycat01 <yipgerald@yahoo.com.sg>
2020-12-17 19:23:58 +0800

    no real change

--- src/components/HomePanel.js
+++ src/components/HomePanel.js
@@ -18,6 +18,7 @@ const HomePanel = (props) => {
             
             <div className="introText" style={{fontSize: props.mobile ? "10pt": "12pt", lineHeight: props.mobile ? "1.3": "1.6"}}>
                 <p>
+                    <h3>Prob insert some updates here</h3>
                     Ut esse eu reprehenderit sunt amet Lorem enim do exercitation dolor dolore cupidatat. Tempor nulla ex fugiat duis anim. Qui adipisicing nisi veniam eu excepteur dolor ad duis anim Lorem commodo laboris. Nostrud mollit cillum velit cillum. Nulla aliqua ad amet elit esse cupidatat est enim ullamco commodo officia labore. In aute fugiat enim tempor do et.
 
                     Amet mollit duis minim dolor sunt nisi magna nulla dolore officia enim esse labore officia. Minim exercitation amet culpa deserunt proident sit laborum. Tempor ad elit minim tempor.
